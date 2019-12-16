e26af5c7fe406b1f7e62a41e94e6f448cf38c112
lts <lts@nd001>
2019-12-16 18:37:48 +0100

    script mvmdbg y tests

---
+++ P3/test/test_relacionales.c
@@ -0,0 +1,19 @@
+//Output
+//    if ( z == 0) print(1); else print(0);
+//    if ( z != 0) print(1); else print(0);
+//    if ( z > 0) print(1); else print(0);
+//    if ( z >= 0) print(1); else print(0);
+//    if ( z < 0) print(1); else print(0);
+//    if ( z <= 0) print(1); else print(0);
+{ int z;
+    while(1)
+    {
+    read(z);
+    if ( z == 0) print(1); else print(0);
+    if ( z != 0) print(1); else print(0);
+    if ( z > 0) print(1); else print(0);
+    if ( z >= 0) print(1); else print(0);
+    if ( z < 0) print(1); else print(0);
+    if ( z <= 0) print(1); else print(0);
+    }
+}
